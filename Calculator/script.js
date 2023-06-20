e45c27ca95ecb35794d0471e7596f64b679d4582
Sabelo-hub <136985273+Sabelo-hub@users.noreply.github.com>
2023-06-20 17:12:35 +0200

    Update script.js

---
+++ Calculator/script.js
@@ -0,0 +1,45 @@
+// doesn't hurt to try, ha ha
+// use onclick() to link javascript and html
+function print1() {
+    console.log(1);
+}
+
+function print2() {
+    console.log(2);
+}
+
+function print3() {
+    console.log(3);
+}
+
+function print4() {
+    console.log(4);
+}
+
+function print5() {
+    console.log(5);
+}
+
+function print6() {
+    console.log(6);
+}
+
+function print7() {
+    console.log(7);
+}
+
+function print8() {
+    console.log(8);
+}
+
+function print9() {
+    console.log(9);
+}
+
+function print1() {
+    console.log(1);
+}
+
+function print1() {
+    console.log(1);
+}
